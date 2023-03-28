0957816261f6d5c29b4d59afb0bb598080c7fb3d
unknown <nguyenducmanh1809@gmail.com>
2023-03-29 00:51:40 +0700

    first commit

---
+++ code.js
@@ -0,0 +1,21 @@
+// let number = 10
+// let resuil = 0
+// resuil = number++
+// console.log(number)
+// console.log(resuil)
+// let bien = [0,1,2];
+// let [item1,item2,item3] = bien
+// console.log(item1,item2,item3)
+// let number1= 10
+// let strNumber= '10'
+// if(number1 == strNumber){
+//     console.log('a')
+// }else{
+//     console.log('b')
+// }
+ let number2='lenh for of'
+// for(const value of number2){
+//     console.log(value)
+// }
+    number2.forEach(value => {
+    console.log(value)})
